af2c136b2a4856bde0ec917a6a4dad094a42a441
skynet <chu_liang_han@hotmail.com>
2024-11-07 12:37:32 +0900

    update

--- scripts/new_model_training.py
+++ scripts/new_model_training.py
@@ -15,23 +15,21 @@ fineweb_dataset = load_dataset("HuggingFaceFW/fineweb", name="sample-10BT", spli
 print("Dataset loaded successfully!")
 
 # Convert dataset to sentence-transformers format
-train_examples = [InputExample(texts=[data['text'], data['text']], label=1.0) for data in tqdm(fineweb_dataset, desc="Creating InputExamples")]
+train_examples = [InputExample(texts=[data['text'], data['text']], label=1.0) for data in tqdm(fineweb_dataset.take(3000000), desc="Creating InputExamples")]
 
 print("Start Data Training...")
 # Define DataLoader and Loss Function
-train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=32)
+train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
 train_loss = losses.CosineSimilarityLoss(model=model)
 
 # Set number of epochs and warmup steps
-num_epochs = 2
+num_epochs = 1
 warmup_steps = int(len(train_dataloader) * num_epochs * 0.1)  # 10% of training steps
 
 print("Fine-tuning model...")
 # Train the model
 model.fit(
     train_objectives=[(train_dataloader, train_loss)],
-    checkpoint_save_steps=10000,
-    steps_per_epoch=len(train_dataloader),
     epochs=num_epochs,
     warmup_steps=warmup_steps
 )
